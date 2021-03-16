98ff058964b17b5db7c90d4da4e6ca5e73c3eea5
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-03-16 11:45:36 -0600

    heroku deployment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const weatherRequest = require('./requests/weather.request');
+const port = 3000;
 
 const app = express();
 
